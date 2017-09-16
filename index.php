765477dbbf1ead3a34e27a0da407946264121a0b
Carlos Patiño <carpat@microsoft.com>
2017-09-16 13:53:04 -0400

    testing

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 		echo (sqlsrv_errors());
 	while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
 		echo ($row['CATEGORY']);
+		echo ("\r\n");
 	}
 	sqlsrv_free_stmt($getResults);
 ?>
