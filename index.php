bf6c782c4422ec1dcae44132ce515d7e8135f0cd
Carlos Patiño <carpat@microsoft.com>
2017-09-15 20:59:02 -0400

    testing2

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
-Name: <input type="text" name="name" value="<?php echo $name;?>">
-
-E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-
-Website: <input type="text" name="website" value="<?php echo $website;?>">
-
-Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-
-Gender:
-<input type="radio" name="gender"
-<?php if (isset($gender) && $gender=="female") echo "checked";?>
-value="female">Female
-<input type="radio" name="gender"
-<?php if (isset($gender) && $gender=="male") echo "checked";?>
-value="male">Male
+    <html>  
+    <head>  
+    <Title>Azure SQL Database - PHP Website</Title>  
+    </head>  
+    <body>  
+    <form method="post" action="?action=add" enctype="multipart/form-data" >  
+    Emp Id <input type="text" name="t_emp_id" id="t_emp_id"/></br>  
+    Name <input type="text" name="t_name" id="t_name"/></br>  
+    Education <input type="text" name="t_education" id="t_education"/></br>  
+    E-mail address <input type="text" name="t_email" id="t_email"/></br>  
+    <input type="submit" name="submit" value="Submit" />  
+    </form> 
+	
+    <?php  
+    /*Connect using SQL Server authentication.*/  
+		$connstr = getenv('SQLAZURECONNSTR_defaultConnection')
+		echo $connstr
+    ?>  
+    </body>  
+    </html>  
