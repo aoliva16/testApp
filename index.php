745131c843778fb28412eedf12adbb778fdbc419
Carlos Patiño <carpat@microsoft.com>
2017-09-16 15:47:32 -0400

    testing

--- index.php
+++ index.php
@@ -6,32 +6,6 @@
 <body>
 <h3> Contact US</h3>
 
-<?php
-
-	require 'connectToDatabase.php';
-
-	$conn = ConnectToDabase()
-
-	// Get data for expense categories
-	$tsql="SELECT CATEGORY FROM Expense_Categories";
-	$expenseCategories= sqlsrv_query($conn, $tsql);
-
-	// Populate dropdown menu 
-	$options = '';
-	while($row = sqlsrv_fetch_array($expenseCategories)) {
-		$options .="<option>" . $row['CATEGORY'] . "</option>";
-	}
-
-	$menu="<form id='filter' name='filter' method='post' action=''>
-	  <p><label>Filter</label></p>
-		<select name='filter' id='filter'>
-		  " . $options . "
-		</select>
-	</form>";
-
-	//echo $menu;
-?>
-
 <form action="thankyou.php" method="post">
 	Name:<br>
 		<input type="text" name="u_name" required><br>
