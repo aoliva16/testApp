ce5acf6446b4fdfd31a3b98c23e05db3dc8f5700
Carlos Patiño <carpat@microsoft.com>
2017-09-16 12:41:47 -0400

    testing

--- index.php
+++ index.php
@@ -10,15 +10,20 @@
 
 <?php
 	$conn = odbc_connect($connStr);
-	$tsql= "SELECT CATEGORY
-			FROM Expense_Categories";
+
+	if($conn)
+        echo "Connected!";
+
+	$tsql= "SELECT CATEGORY FROM Expense_Categories";
 	
 	$getResults= sqlsrv_query($conn, $tsql);
+
 	echo ("Reading data from table" . PHP_EOL);
 	if ($getResults == FALSE)
-		echo (sqlsrv_errors());
+		die(FormatErrors(sqlsrv_errors()));
 	while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
 		echo ($row['CATEGORY'] . PHP_EOL);
+
 	}
 	sqlsrv_free_stmt($getResults);
 ?>
