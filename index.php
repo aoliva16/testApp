cdb5e619cec09c30013d20422608f346661823a8
Carlos Patiño <carpat@microsoft.com>
2017-09-16 15:49:22 -0400

    testing

--- index.php
+++ index.php
@@ -6,11 +6,36 @@
 <body>
 <h3> Contact US</h3>
 
+<?php
+
+	require 'connectToDatabase.php';
+	$conn = ConnectToDabase()
+
+	// Get data for expense categories
+	$tsql="SELECT CATEGORY FROM Expense_Categories";
+	$expenseCategories= sqlsrv_query($conn, $tsql);
+
+	// Populate dropdown menu 
+	$options = '';
+	while($row = sqlsrv_fetch_array($expenseCategories)) {
+		$options .="<option>" . $row['CATEGORY'] . "</option>";
+	}
+
+	$menu="<form id='filter' name='filter' method='post' action=''>
+	  <p><label>Filter</label></p>
+		<select name='filter' id='filter'>
+		  " . $options . "
+		</select>
+	</form>";
+
+	//echo $menu;
+?>
+
 <form action="thankyou.php" method="post">
 	Name:<br>
 		<input type="text" name="u_name" required><br>
  
-	Email:
+	Email:<br>
 		<input type="email" name="u_email" required><br>
  
 	Subject:<br>
@@ -19,11 +44,9 @@
 	Message:<br>
 		<input type="text" name="message" required><br>
 
-	<label>Fruits</label>
+	<label>Fruits</label><br>
 		<select name="Fruits">
-		<option value="Ap">Apple</option>
-		<option value="BN">Banana</option>
-		<option value="OR">Orange</option>
+		 " . $options . "
 
 <input type="submit" value="Submit"><br>
 </form>
