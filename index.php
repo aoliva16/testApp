f085ddcaf4c33b573ed1c55fda6d0c0bcc3288ae
Carlos Patiño <carpat@microsoft.com>
2017-09-16 11:23:42 -0400

    testing

--- index.php
+++ index.php
@@ -3,12 +3,10 @@
 <body>
 
 <?php  
-	$connstr = getenv('SQLAZURECONNSTR_defaultConnection')
-	
 	echo "Hello world"
 ?>
 
-<tr><td> <input type="hidden" name="type" value="<?= $connstr ?>" ></td></tr>
+
 
 </body>
 </html>
