fb3d7c2f5a92a562a57834e37831bd62c41311b6
Carlos Patiño <carpat@microsoft.com>
2017-09-15 21:01:17 -0400

    testing3

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
-    <html>  
-    <head>  
-    <Title>Azure SQL Database - PHP Website</Title>  
-    </head>  
-    <body>  
-    <form method="post" action="?action=add" enctype="multipart/form-data" >  
-    Emp Id <input type="text" name="t_emp_id" id="t_emp_id"/></br>  
-    Name <input type="text" name="t_name" id="t_name"/></br>  
-    Education <input type="text" name="t_education" id="t_education"/></br>  
-    E-mail address <input type="text" name="t_email" id="t_email"/></br>  
-    <input type="submit" name="submit" value="Submit" />  
-    </form> 
-	
-    <?php  
-    /*Connect using SQL Server authentication.*/  
-		$connstr = getenv('SQLAZURECONNSTR_defaultConnection')
-		echo $connstr
-    ?>  
-    </body>  
-    </html>  
+<?php  
+	$connstr = getenv('SQLAZURECONNSTR_defaultConnection')
+	echo $connstr
+?>
