d262ca76e375d7c18c4103557fdbf110bef44055
Carlos Patiño <carpat@microsoft.com>
2017-09-16 13:34:40 -0400

    testing

--- index.php
+++ index.php
@@ -11,13 +11,11 @@
 	$tsql="SELECT CATEGORY FROM Expense_Categories";
 	$getResults= sqlsrv_query($conn, $tsql);
 
-	echo $getResults
-
-	echo ("Reading data from table" . PHP_EOL);
+	echo ("Reading data from table");
 	if ($getResults == FALSE)
 		echo (sqlsrv_errors());
 	while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-		echo ($row['CATEGORY'] . PHP_EOL);
+		echo ($row['CATEGORY']);
 	}
 	sqlsrv_free_stmt($getResults);
 ?>
