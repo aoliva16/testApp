b6c4385b4d9cdea78230eab232026015f7549a5a
Carlos Patiño <carpat@microsoft.com>
2017-09-17 13:36:04 -0400

    testing

--- insertToDb.php
+++ insertToDb.php
@@ -27,12 +27,12 @@
 
 	$tsql="SELECT CATEGORY FROM Expense_Categories";
 
-	echo $expenseDay .PHP_EOL. ;
-	echo $expenseMonth .PHP_EOL. ;
-	echo $expenseYear .PHP_EOL. ;
-	echo $expenseAmount .PHP_EOL. ;
-	echo $expenseNote .PHP_EOL. ;
-	echo $expenseCategory .PHP_EOL. ;
+	echo $expenseDay;
+	echo $expenseMonth;
+	echo $expenseYear;
+	echo $expenseAmount;
+	echo $expenseNote;
+	echo $expenseCategory;
 
 	// Close SQL database connection
 	sqlsrv_close ($conn);
