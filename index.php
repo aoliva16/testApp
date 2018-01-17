b0124711dc23cf7fe435318f7c7a98ca6dc926d9
Carlos Patiño <carpat@microsoft.com>
2018-01-17 00:00:18 -0500

    Adding HTML enconding info

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en-US"> 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	<title> 
 		Carlos' Expense Input Tool 
 	</title>
